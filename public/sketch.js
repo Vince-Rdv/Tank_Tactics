e33905fee8c623c91a87c782f6e560c65b3e9eab
Vince-Rdv <vincent15vanderree@gmail.com>
2022-01-02 21:08:42 +0100

    Created gameState starting and Discord interaction

--- public/sketch.js
+++ public/sketch.js
@@ -1,7 +1,63 @@
-function setup(){
+var socket = io();
+
+function setup() {
     createCanvas(1920, 1080);
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const page_id = urlParams.get('id')
+    socket.emit('getGamestate', {id: page_id});
 }
 
-function draw(){
+var gameState = {}
+var gameStateDataReceived = false;
+
+socket.on('getGamestateResponse', (res) => {
+    gameStateDataReceived = true;
+    gameState = res;
+    boardWidth = gameState.gameBoard.length;
+    boardHeight = gameState.gameBoard[0].length;
+});
+
+var boardWidth = 0;
+var boardHeight = 0;
+
+function draw() {
     background(51);
+
+    if (gameStateDataReceived) {
+        push();
+        translate(20, 20)
+        fill(80)
+        rect(0, 0, width - 40, height - 40)
+        for (var x = 0; x < boardWidth; x++) {
+            line(Math.round(x * ((width - 40) / boardWidth)), 0, Math.round(x * ((width - 40) / boardWidth)), height - 40)
+        }
+        for (var y = 0; y < boardHeight; y++) {
+            line(0, Math.round(y * ((height - 40) / boardHeight)), width - 40, Math.round(y * ((height - 40) / boardHeight)))
+        }
+
+        // line(Math.round(y * ((height - 40) / boardHeight)), 0, Math.round(y * ((height - 40) / boardHeight)), height - 40)
+        pop();
+
+    } else {
+        textSize(64)
+        textAlign(CENTER, CENTER)
+        fill(255)
+        if (Math.floor(frameCount / 40) % 3 == 0) text("Loading.", width / 2, height / 2)
+        if (Math.floor(frameCount / 40) % 3 == 1) text("Loading..", width / 2, height / 2)
+        if (Math.floor(frameCount / 40) % 3 == 2) text("Loading...", width / 2, height / 2)
+    }
 }
+
+function drawPlayerStats(p) {
+    var boxHeight = (boardHeight / players.length)
+    fill(0)
+    textSize(boxHeight / 4)
+    text(players[p].name, 8, boxHeight / 4 + 4)
+    if (true) { //TODO Create current player setting in player file
+        noFill();
+        rect(4, 4, ((width - boardWidth - 20 - 40)) - 8, (boardHeight / players.length) - 8)
+
+    }
+}
+
--- server.js
+++ server.js
@@ -4,9 +4,8 @@ var config = {
 
 // Random libraries
 var colors = require('colors'); // Colored console output (colors is not used, require extends the string Prototype)
-
 console.clear();
-
+var simpleLog = false; //Don't log linenumber and time
 function vLog(type, message) {
 
     var succes = true;
@@ -59,7 +58,11 @@ function vLog(type, message) {
     output += message
 
     if (succes) {
-        console.log(output)
+        if (simpleLog) {
+            console.log(message)
+        } else {
+            console.log(output)
+        }
     } else {
         //Incorrect log type
         console.log(("Warning, incorrect type logged at " + lineNumber.line).bgRed.white)
@@ -72,28 +75,131 @@ function vLog(type, message) {
     }
 
 }
-// Logo on boot in console
-console.log("*************************************************************************************\n*        _____                _      _____               _    _                     *\n*       |_   _|              | |    |_   _|             | |  (_)                    *\n*         | |    __ _  _ __  | | __   | |    __ _   ___ | |_  _   ___  ___          *\n*         | |   / _` || '_ \\ | |/ /   | |   / _` | / __|| __|| | / __|/ __|         *\n*         | |  | (_| || | | ||   <    | |  | (_| || (__ | |_ | || (__ \\__ \\         *\n*         \\_/   \\__,_||_| |_||_|\\_\\   \\_/   \\__,_| \\___| \\__||_| \\___||___/         *\n*                                                                                   *\n*                                                                                   *\n*    _   _                   _                 __      _____     _____     _____    *\n*   | | | |                 (_)               /  |    |  _  |   |  _  |   |  _  |   *\n*   | | | |  ___  _ __  ___  _   ___   _ __   `| |    | |/' |   | |/' |   | |/' |   *\n*   | | | | / _ \\| '__|/ __|| | / _ \\ | '_ \\   | |    |  /| |   |  /| |   |  /| |   *\n*   \\ \\_/ /|  __/| |   \\__ \\| || (_) || | | | _| |_ _ \\ |_/ / _ \\ |_/ / _ \\ |_/ /   *\n*    \\___/  \\___||_|   |___/|_| \\___/ |_| |_| \\___/(_) \\___/ (_) \\___/ (_) \\___/    *\n*                                                                                   *\n*************************************************************************************")
 
-vLog("info", "Tank Tactics server starting")
-
+vLog("log", "Tank Tactics server starting")
+// Express Setup
 const express = require('express');
 const app = express();
 const http = require('http');
-vLog("info", "Express server booted up")
+vLog("log", "Express server booted up")
 
+// Socket.io Setup
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-vLog("info", "Socket.io server booted up")
+vLog("log", "Socket.io server booted up")
+
+// Discord.js Setup
+var token = "NDM3NjY2MjExMTQ5NTEyNzE0.WtzGbA.gLgTfL5XGtF8biYI6V-9GS7ntKM"
+
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES] }); //create new client
 
 app.use(express.static('public'))
-vLog("info", "Changed Express to serve in static mode")
+vLog("log", "Changed Express to serve in static mode")
 
+// Socket.io Run
 io.on('connection', (socket) => {
-    vLog("info", "A new user connected through socket.io")
-});
+    vLog("log", "A new user connected through socket.io")
 
+    socket.on("getGamestate", (res) => { // Board information
+        vLog("player", "Player " + playerNames[res.id] + " has requested the game state");
+        var gameState = {
+            "players": playerList,
+            "gameBoard": gameBoard
+        }
+        socket.emit("getGamestateResponse", gameState)
+    });
+});
+// Express Run
 server.listen(config.port, () => {
     vLog("info", "Express listening on port " + config.port)
 });
+
+// Discord.js Run
+client.once('ready', () => {
+    vLog("info", "Discord logged in")
+});
+
+client.on('messageCreate', function(msg){
+    console.log(msg)
+    if(msg.content == "register"){
+        msg.reply('Hello ' + msg.author.username + '. I have just registered you for this round of Tank Tac Toe. Once the game starts you will receive a private message from me');
+        playerList.push(createPlayer(createID(), msg.author.username, 0, 0))
+    }
+});
+
+client.login(token)
+
+
+
+var playerNames = ["Alpha", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India", "Juliet", "Kilo", "Lima", "Mike", "November", "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango", "Uniform", "Victor", "Whiskey", "X-Ray", "Yankee", "Zulu"]
+
+var gameBoard = [];
+var playerList = [];
+
+/**
+ * This function boots up all functions to setup before the game 
+ * @method
+ * @param {number} boardWidth Board width in tiles
+ * @param {number} boardHeight Board height in tiles
+ * @param {number} players Amount of players in the game
+ * @param {*} options Other options 
+ */
+function initGame(boardWidth = 10, boardHeight = 10, players = 4, options = {}) {
+    vLog("info", "Board creation started")
+    gameBoard = createBoard(boardWidth, boardHeight)
+}
+
+/**
+ * Create the board array
+ * @method
+ * @param {number} boardWidth Board width in tiles
+ * @param {number} boardHeight Board height in tiles
+ * @returns Array of board
+ */
+function createBoard(boardWidth, boardHeight) {
+    var temp = [];
+    for (var x = 0; x < boardWidth; x++) {
+        temp[x] = []
+        for (var y = 0; y < boardHeight; y++) {
+            temp[x][y] = Math.ceil(Math.random() * 50)
+        }
+    }
+    return temp
+}
+
+/**
+ * Create a player object
+ * @method
+ * @param {number} id 
+ * @param {string} name 
+ * @param {number} x 
+ * @param {number} y 
+ * @param {*} options 
+ * @returns Player object
+ */
+function createPlayer(id, name, x = 0, y = 0, options = {}) {
+    var temp = {
+        x: x,
+        y: y,
+        id: id,
+        health: 3,
+        actionPoints: 3,
+        shootRange: 3,
+        options: options
+    }
+    return temp
+}
+var tempH = 20;
+var tempW = Math.floor(tempH * 1.80769230769)
+initGame(tempW, tempH, 10);
+
+function createID(){
+    var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    var randomID = "";
+    for(var x = 0; x < 64; x++){
+        randomID += chars.charAt(Math.floor(Math.random() * chars.length))
+    }
+    return randomID
+}
